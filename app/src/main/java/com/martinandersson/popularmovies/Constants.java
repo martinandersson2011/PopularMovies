2495e768c77118cbb3cd185deaaf4826d55d82fc
Martin Andersson <martinandersson2011@gmail.com>
2015-10-10 09:24:57 -0500

    Placeholder for themoviedb API key.

---
+++ app/src/main/java/com/martinandersson/popularmovies/Constants.java
@@ -0,0 +1,11 @@
+package com.martinandersson.popularmovies;
+
+public final class Constants {
+
+    private Constants() {
+    }
+
+    // Replace with your own key. You can get it from https://www.themoviedb.org/
+    public static final String THE_MOVIE_DB_API_KEY = "REPLACE_WITH_YOUR_API_KEY";
+
+}
